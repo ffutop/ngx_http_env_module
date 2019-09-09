2349d8e91cbf4d677357e4caa1eadf2f9b278401
ffutop <17816873960@163.com>
2019-09-09 20:54:10 +0800

    update srv conf of env module.

--- ngx_http_env_module.c
+++ ngx_http_env_module.c
@@ -3,54 +3,33 @@
 #include <ngx_http.h>
 
 typedef struct {
-    ngx_str_t deployment_env;
-} ngx_http_env_main_conf_t;
+    ngx_str_t *deployment_env;
+} ngx_http_env_srv_conf_t;
 
-static void * ngx_http_env_create_main_conf(ngx_conf_t *cf) {
-    ngx_http_env_main_conf_t *conf;
-
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_env_main_conf_t));
+static void * ngx_http_env_create_srv_conf(ngx_conf_t *cf) {
+    ngx_http_env_srv_conf_t *conf;
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_env_srv_conf_t));
     if (conf == NULL) {
         return NULL;
     }
-
-    ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "init main conf");
-
-    ngx_str_null(&conf->deployment_env);
+    ngx_conf_log_error(NGX_LOG_INFO, cf, 0, "init srv conf");
     return conf;
 }
 
-static char * ngx_http_env_init_main_conf(ngx_conf_t *cf, void *conf) {
-    ngx_http_env_main_conf_t *emcf = conf;
-
-    ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "init main conf");
-    ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "init main conf. env: %d", emcf->deployment_env.len);
-
+static char * ngx_http_env_merge_srv_conf(ngx_conf_t *cf, void *parent, void *child) {
+    ngx_http_env_srv_conf_t *prev = parent;
+    ngx_http_env_srv_conf_t *conf = child;
+    ngx_conf_merge_str_value(conf->deployment_env, conf->deployment_env, "");
+    ngx_conf_log_error(NGX_LOG_INFO, cf, 0, "merge srv conf");
     return NGX_CONF_OK;
-}
-
-char * ngx_http_deployment_env(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
-    ngx_http_env_main_conf_t *emcf = conf;
-
-    ngx_str_t *value;
-
-    value = cf->args->elts;
-    if (cf->args->nelts == 2) {
-        ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "value: %V %V", value, value+1);
-        ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "set deployment_env");
-        emcf->deployment_env.len = (value+1)->len;
-        emcf->deployment_env.data = (value+1)->data;
-        return NGX_CONF_OK;
-    }
-    return NGX_CONF_ERROR;
 }
 
 static ngx_command_t ngx_http_env_commands[] = {
     { ngx_string("deployment_env"),
-      NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
-      ngx_http_deployment_env,
-      NGX_HTTP_MAIN_CONF_OFFSET,
-      offsetof(ngx_http_env_main_conf_t, deployment_env),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_str_slot,
+      NGX_HTTP_SRV_CONF_OFFSET,
+      offsetof(ngx_http_env_srv_conf_t, deployment_env),
       NULL},
     ngx_null_command
 };
@@ -59,11 +38,11 @@ static ngx_http_module_t ngx_http_env_module_ctx = {
     NULL,                           /* preconfiguration */
     NULL,                           /* postconfiguration */
 
-    ngx_http_env_create_main_conf,  /* create main configuration */
-    ngx_http_env_init_main_conf,    /* init main configuration */
+    NULL,                           /* create main configuration */
+    NULL,                           /* init main configuration */
 
-    NULL,                           /* create server configuration */
-    NULL,                           /* merge server configuration */
+    ngx_http_env_create_srv_conf,   /* create server configuration */
+    ngx_http_env_merge_srv_conf,    /* merge server configuration */
 
     NULL,                           /* create location configuration */
     NULL                            /* merge location configuration */
